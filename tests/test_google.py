ff573835a8911034e63be62e7c9c739c95de3bdd
keyurpatil06 <keyurpatil3000@gmail.com>
2025-09-04 21:42:55 +0530

    Remove old test.py file

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,11 +1,8 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 def test_open_google():
     driver = webdriver.Chrome()
     driver.get("https://www.google.com")
-
     assert "Google" in driver.title
-
     driver.quit()

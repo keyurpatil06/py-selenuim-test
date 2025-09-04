636f8d864918d32e355e2441b0fcefb1d1c4bf2b
Keyur Patil <122732152+keyurpatil06@users.noreply.github.com>
2025-09-04 21:30:33 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -3,11 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 def test_open_google():
-    # Start Chrome (make sure chromedriver.exe is in PATH)
     driver = webdriver.Chrome()
     driver.get("https://www.google.com")
 
-    # Verify page title contains 'Google'
     assert "Google" in driver.title
 
     driver.quit()
